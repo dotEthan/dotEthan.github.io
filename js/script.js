6a54afed98f468b22452a47b66ab0fad9d880528
Ethan Strauss <estrauss@gmail.com>
2018-07-23 16:09:39 -0400

    figured out the layout

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const areaGrid = document.querySelectorAll('.area__contain');
+
+document.querySelector('.content__contain').addEventListener('click', openSaysMe);
+
+function openSaysMe(e) {
+	const hiding = document.querySelector('.hide');
+	console.log(e.target);
+	if(!e.target.id) return;
+	if(e.target.id === 'hideit') hiding.classList.remove('unhide');
+	e.target.classList.add('open');
+	hiding.classList.add('unhide');
+}
