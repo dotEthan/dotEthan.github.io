56501de94532d73aa1458c516d4ea0a8442e6ab8
Ethan Strauss <estrauss@gmail.com>
2018-09-04 22:22:05 -0400

    not there yet, but gallery is coming

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+
+//CLICKING BETWEEN THE PANELS
+
+
 const drawers = document.querySelectorAll('.drawer');
 const titles = document.querySelectorAll('.title__contain');
 const areas = document.querySelectorAll('.area__contain');
@@ -65,11 +69,17 @@ window.onload = () => {
 	const overlay = document.querySelector('.overlay');
 	const overlayImage = overlay.querySelector('img');
 	const overlayClose = overlay.querySelector('.close');
+	const windowSize = (Modernizr.mq('(min-width: 768px)')) ? 'big' : 'small';
 	let nowr = [0, 0, 0, 0, 0];
 	let oriArr = [1,2,3,4,5];
 	let horzArr = [...oriArr];
 	let vertArr = [...oriArr];
 	let squareArr = [...oriArr];
+	let imgArr = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,1,1,0,0,0,0],[0,0,0,0,1,1,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]; 
+	let imgArrSm = [[0,0,0,0],[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,0,0,0],[0,0,0,0]];
+	let testArr = (Modernizr.mq('(min-width: 768px)')) ? [...imgArr] : [...imgArrSm];
+	// console.log(testArr);
+	// console.log(imgArr);
 
 	document.onkeydown = function(evt) {
 		evt = evt || window.event;
@@ -105,7 +115,7 @@ window.onload = () => {
 		          </div>
 		        </div>
 		      `;			
-		} else {
+		} else if (v === h){
 			return `
 		        <div class="item h${h} v${v}">
 		          <img src="images/sq/${randomUniqueNumber(oriArr.length,'square')}.jpg">
@@ -118,10 +128,9 @@ window.onload = () => {
 	}
 
 	function randomUniqueNumber(limit,type) {
+		console.log('random');
 		let thisNum = 0;
 		if (type === 'vert') {
-			console.log('vert');
-			console.log(vertArr);
 
 			if (vertArr.length === 0) vertArr = [...oriArr];
 
@@ -132,12 +141,10 @@ window.onload = () => {
 			const numInd = vertArr.indexOf(thisNum);
 
 			vertArr.splice(numInd, 1);
-
+			console.log(thisNum);
 			return thisNum;
 
 		} else if (type === 'horz') {
-			console.log('horz');
-			console.log(horzArr);
 
 			if (horzArr.length === 0) horzArr = [...oriArr];
 
@@ -148,12 +155,11 @@ window.onload = () => {
 			const numInd = horzArr.indexOf(thisNum);
 
 			horzArr.splice(numInd, 1);
+			console.log(thisNum);
 
 			return thisNum;
 
 		} else if (type === 'square') {	
-			console.log('quare');
-			console.log(squareArr);
 
 			if (squareArr.length === 0) squareArr = [...oriArr];
 
@@ -164,6 +170,7 @@ window.onload = () => {
 			const numInd = squareArr.indexOf(thisNum);
 
 			squareArr.splice(numInd, 1);
+			console.log(thisNum);
 
 			return thisNum;
 
@@ -185,10 +192,110 @@ window.onload = () => {
 		overlay.classList.remove('open');
 	}
 
-	const digits = Array.from({ length: 15 }, () => [
-		randomNumber(3),
-		randomNumber(3)
-	]).concat([[1, 1],[1, 1],[1, 1],[1, 1],[1, 1]]);
+	const digits = Array.from({ length: 15 }, randomNumArr);
+
+	function randomNumArr () {
+		console.count('random');
+		let testDigits =  [ randomNumber(3), randomNumber(3) ], fullRow = 0, full = false;
+
+		console.log(`test: 
+			${testArr[0]}
+			${testArr[1]}
+			${testArr[2]}
+			${testArr[3]}
+			`);
+		console.log(testDigits);
+ 
+		testArr.forEach((row) => {
+			if(!row.includes(0)) {
+				fullRow++;
+			}
+			if(fullRow === 4) {
+				full = true;
+			}
+			console.log(fullRow);
+		});
+
+		if (willFit(testDigits[0], testDigits[1]) && !full) {
+			console.log('yay');
+			return testDigits;	
+		} else if (full) {
+			console.log('nope');
+			return [0,0];
+		} else {
+			console.log('boo');
+			randomNumArr();
+		}
+		console.log(testDigits);
+		return testDigits;
+
+	}
+
+
+	
+	function willFit(h,v) {
+		// testArr = [[1,1,1,1,0,0,1,1,1,1],[1,1,0,1,1,1,1,1,1,1],[1,1,0,0,1,1,1,1,1,0],[0,0,0,0,0,0,1,1,1,0]]; 
+		// console.log(h, v);
+		let testH = h, testV = v, fits = false;
+
+		rowLoop: 
+		for(let i = 0; i <= testArr.length-1; i ++) {
+			columnLoop:
+			for(let j = 0; j <= testArr[i].length-1; j ++) {
+				// console.log(testH, testV);
+
+				if(testArr[i][j]===0) {
+					let throwV = testV;
+					while(throwV >= 1) {
+						let throwH = testH
+						// console.log(throwV);
+						// console.log(throwH);
+						// console.log(testH);
+						while (throwH >= 1) {
+							// console.log(testArr[i + (throwV-1)][j + (throwH-1)]);
+							if(testArr[i + (throwV-1)] === undefined || testArr[i + (throwV-1)][j + (throwH-1)] !== 0 ) {
+								// console.log('now');
+								continue columnLoop;
+							}
+							throwH--;
+						};
+					throwV--
+					};
+
+					fits=true;
+
+					while(v >= 1) {
+						let throwH = h;
+						// console.log(throwH);
+						while (throwH >= 1) {
+							// console.log(v);
+							if(testArr[i + (v-1)] === undefined) continue columnLoop;
+							testArr[i + (v-1)][j + (throwH-1)] = 1;
+							throwH--;
+						};
+					v--;
+					};
+					break rowLoop;
+				}
+			}
+		}
+
+		// console.log(`test: 
+		// 	${testArr[0]}
+		// 	${testArr[1]}
+		// 	${testArr[2]}
+		// 	${testArr[3]}
+		// 	`);
+		// console.log(fits);
+		return fits;
+	}
+
+
+	// willFit(1,1);
+	// willFit(1,1);
+
+	// console.log(digits);
+
 
 	const html = digits.map(generateHTML).join('');
 
@@ -199,4 +306,5 @@ window.onload = () => {
 	items.forEach(item => item.addEventListener('click', showPic));
 
 	overlayClose.addEventListener('click', closeImage);
+	willFit(3, 3);
 };
