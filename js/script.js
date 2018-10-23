24b533bc0b8a0ccdd8e0b0c052a5d1e334a7ba04
Ethan Strauss <estrauss@gmail.com>
2018-10-23 11:04:00 -0400

    Babelfying, seeing how broken IE is.

--- js/script.js
+++ js/script.js
@@ -1,52 +1,94 @@
 "use strict";
 
-const drawers = document.querySelectorAll(".drawer");
-const titles = document.querySelectorAll(".title__contain");
-const areas = document.querySelectorAll(".area__contain");
-const areasL = areas.length;
+var _slicedToArray = (function() {
+  function sliceIterator(arr, i) {
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _e = undefined;
+    try {
+      for (
+        var _i = arr[Symbol.iterator](), _s;
+        !(_n = (_s = _i.next()).done);
+        _n = true
+      ) {
+        _arr.push(_s.value);
+        if (i && _arr.length === i) break;
+      }
+    } catch (err) {
+      _d = true;
+      _e = err;
+    } finally {
+      try {
+        if (!_n && _i["return"]) _i["return"]();
+      } finally {
+        if (_d) throw _e;
+      }
+    }
+    return _arr;
+  }
+  return function(arr, i) {
+    if (Array.isArray(arr)) {
+      return arr;
+    } else if (Symbol.iterator in Object(arr)) {
+      return sliceIterator(arr, i);
+    } else {
+      throw new TypeError(
+        "Invalid attempt to destructure non-iterable instance"
+      );
+    }
+  };
+})();
+
+var drawers = document.querySelectorAll(".drawer");
+var titles = document.querySelectorAll(".title__contain");
+var areas = document.querySelectorAll(".area__contain");
+var areasL = areas.length;
 
 // Open Each Panel
 
-titles.forEach(title => title.addEventListener("click", openSaysMe));
+titles.forEach(function(title) {
+  return title.addEventListener("click", openSaysMe);
+});
 
 function openSaysMe(e) {
-  const id = "#" + e.target.id;
-  const titleId = "#" + e.target.id;
-  const ele = document.querySelector(
+  var id = "#" + e.target.id;
+  var titleId = "#" + e.target.id;
+  var ele = document.querySelector(
     id.replace("-area", "").replace("-title", "")
   );
-  let inde;
+  var inde = void 0;
 
-  const titleEle = document.querySelector(id.replace("-title", "-area"));
+  var titleEle = document.querySelector(id.replace("-title", "-area"));
 
   // Big Screens
 
   if (Modernizr.mq("(min-width: 768px)")) {
-    areas.forEach((area, i) => {
+    areas.forEach(function(area, i) {
       if (titleEle.id === area.id) {
         inde = i;
       }
     });
 
     if (areas[inde].classList.contains("open")) {
-      for (let i = areasL - 1; i >= inde; i--) {
+      for (var i = areasL - 1; i >= inde; i--) {
         areas[i].classList.remove("open");
       }
     } else {
-      for (let i = 0; i < areasL; i++) {
-        if (i <= inde) {
-          areas[i].classList.add("open");
+      for (var _i = 0; _i < areasL; _i++) {
+        if (_i <= inde) {
+          areas[_i].classList.add("open");
         } else {
-          areas[i].classList.remove("open");
+          areas[_i].classList.remove("open");
         }
       }
     }
   } else {
     // Small screens
 
-    let same = false;
+    var same = false;
 
-    drawers.forEach(drawer => {
+    drawers.forEach(function(drawer) {
       if (e.target.id && ele.classList.contains("open")) {
         ele.classList.remove("open");
         same = true;
@@ -64,26 +106,26 @@ function openSaysMe(e) {
 
 // Gallery Creation
 
-window.onload = () => {
-  const gallery = document.querySelector(".gallery");
-  const container = document.querySelector(".container");
-  const overlay = document.querySelector(".overlay");
-  const overlayImage = overlay.querySelector("img");
-  const overlayClose = overlay.querySelector(".close");
-  const windowSize = Modernizr.mq("(min-width: 768px)") ? "big" : "small";
-  let nowr = [0, 0, 0, 0, 0];
-  let oriArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  let horzArr = [...oriArr];
-  let vertArr = [...oriArr];
-  let squareArr = [...oriArr];
+window.onload = function() {
+  var gallery = document.querySelector(".gallery");
+  var container = document.querySelector(".container");
+  var overlay = document.querySelector(".overlay");
+  var overlayImage = overlay.querySelector("img");
+  var overlayClose = overlay.querySelector(".close");
+  var windowSize = Modernizr.mq("(min-width: 768px)") ? "big" : "small";
+  var nowr = [0, 0, 0, 0, 0];
+  var oriArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  var horzArr = [].concat(oriArr);
+  var vertArr = [].concat(oriArr);
+  var squareArr = [].concat(oriArr);
 
-  let imgArr = [
+  var imgArr = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
-  let imgArrSm = [
+  var imgArrSm = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 1, 1, 0],
@@ -91,9 +133,9 @@ window.onload = () => {
     [0, 0, 0, 0],
     [0, 0, 0, 0]
   ];
-  let testArr = Modernizr.mq("(min-width: 768px)")
-    ? [...imgArr]
-    : [...imgArrSm];
+  var testArr = Modernizr.mq("(min-width: 768px)")
+    ? [].concat(imgArr)
+    : [].concat(imgArrSm);
 
   // Close overlay on Esc keydown
 
@@ -113,35 +155,39 @@ window.onload = () => {
 
   // Create HTML for each Picture
 
-  function generateHTML([h, v]) {
-    let folder = "square";
+  function generateHTML(_ref) {
+    var _ref2 = _slicedToArray(_ref, 2),
+      h = _ref2[0],
+      v = _ref2[1];
+
+    var folder = "square";
 
     if (h > v) folder = "horz";
     if (h < v) folder = "vert";
 
-    return `
-	        <div class="item h${h} v${v}">
-	          <img alt="Images from Ethans life" src="https://res.cloudinary.com/dotethan/image/upload/v1537990671/Portfolio/${folder}/${randomUniqueNumber(
-      oriArr.length,
-      folder
-    )}.jpg">
-	          <div class="item__overlay">
-	            <button>View ↪</button>
-	          </div>
-	        </div>
-	      `;
+    return (
+      '\n\t        <div class="item h' +
+      h +
+      " v" +
+      v +
+      '">\n\t          <img alt="Images from Ethans life" src="https://res.cloudinary.com/dotethan/image/upload/v1537990671/Portfolio/' +
+      folder +
+      "/" +
+      randomUniqueNumber(oriArr.length, folder) +
+      '.jpg">\n\t          <div class="item__overlay">\n\t            <button>View \u21AA</button>\n\t          </div>\n\t        </div>\n\t      '
+    );
   }
 
   // Ensure pictures aren't duplicated too often
 
   function updateArr(arrName, limit) {
-    let thisNum = 0;
+    var thisNum = 0;
 
     do {
       thisNum = Math.floor(Math.random() * limit) + 1;
     } while (!arrName.includes(thisNum));
 
-    const numInd = arrName.indexOf(thisNum);
+    var numInd = arrName.indexOf(thisNum);
     arrName.splice(numInd, 1);
 
     return thisNum;
@@ -151,15 +197,15 @@ window.onload = () => {
 
   function randomUniqueNumber(limit, type) {
     if (type === "vert") {
-      if (vertArr.length === 0) vertArr = [...oriArr];
+      if (vertArr.length === 0) vertArr = [].concat(oriArr);
 
       return updateArr(vertArr, limit);
     } else if (type === "horz") {
-      if (horzArr.length === 0) horzArr = [...oriArr];
+      if (horzArr.length === 0) horzArr = [].concat(oriArr);
 
       return updateArr(horzArr, limit);
     } else if (type === "square") {
-      if (squareArr.length === 0) squareArr = [...oriArr];
+      if (squareArr.length === 0) squareArr = [].concat(oriArr);
 
       return updateArr(squareArr, limit);
     }
@@ -172,7 +218,7 @@ window.onload = () => {
   //Show the picture
 
   function showPic(e) {
-    const src = e.currentTarget.querySelector("img").src;
+    var src = e.currentTarget.querySelector("img").src;
     overlayImage.src = src;
     overlay.classList.add("open");
   }
@@ -183,14 +229,14 @@ window.onload = () => {
 
   //create an array of Horizontal and Vertical numbers to be tested
 
-  const digits = Array.from({ length: 20 }, randomNumArr);
+  var digits = Array.from({ length: 20 }, randomNumArr);
 
   function randomNumArr() {
-    let testDigits = [randomNumber(3), randomNumber(3)],
+    var testDigits = [randomNumber(3), randomNumber(3)],
       fullRow = 0,
       full = false;
 
-    testArr.forEach(row => {
+    testArr.forEach(function(row) {
       if (!row.includes(0)) {
         fullRow++;
       }
@@ -212,17 +258,17 @@ window.onload = () => {
   // Test to make sure no image is going to extend out of the element.
 
   function willFit(h, v) {
-    let testH = h,
+    var testH = h,
       testV = v,
       fits = false;
 
-    rowLoop: for (let i = 0; i <= testArr.length - 1; i++) {
-      columnLoop: for (let j = 0; j <= testArr[i].length - 1; j++) {
+    rowLoop: for (var i = 0; i <= testArr.length - 1; i++) {
+      columnLoop: for (var j = 0; j <= testArr[i].length - 1; j++) {
         if (testArr[i][j] === 0) {
-          let throwV = testV;
+          var throwV = testV;
 
           while (throwV >= 1) {
-            let throwH = testH;
+            var throwH = testH;
 
             while (throwH >= 1) {
               if (
@@ -241,11 +287,11 @@ window.onload = () => {
           fits = true;
 
           while (v >= 1) {
-            let throwH = h;
-            while (throwH >= 1) {
+            var _throwH = h;
+            while (_throwH >= 1) {
               if (testArr[i + (v - 1)] === undefined) continue columnLoop;
-              testArr[i + (v - 1)][j + (throwH - 1)] = 1;
-              throwH--;
+              testArr[i + (v - 1)][j + (_throwH - 1)] = 1;
+              _throwH--;
             }
             v--;
           }
@@ -258,13 +304,15 @@ window.onload = () => {
 
   // Create Gallery HTML and apply
 
-  const html = digits.map(generateHTML).join("");
+  var html = digits.map(generateHTML).join("");
 
   gallery.innerHTML = gallery.innerHTML + html;
 
-  const items = document.querySelectorAll(".item");
+  var items = document.querySelectorAll(".item");
 
-  items.forEach(item => item.addEventListener("click", showPic));
+  items.forEach(function(item) {
+    return item.addEventListener("click", showPic);
+  });
 
   overlayClose.addEventListener("click", closeImage);
 };
