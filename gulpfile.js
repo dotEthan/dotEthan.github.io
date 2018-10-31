0069c714fad37be70d57affd31e0768b1e639b14
Ethan Strauss <estrauss@gmail.com>
2018-10-31 18:29:07 -0400

    99% done list.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,32 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
-const sass = require("gulp-sass");
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const csswring = require('csswring');
+var autoprefixer = require('autoprefixer');
+const postcssPresetEnv = require('postcss-preset-env');
 
-// Compile Sass & Inject Into Browser
-gulp.task("sass", function() {
-  return gulp
-    .src(["scss/*.scss"])
+gulp.task('styles', function () {
+  const processors = [
+    // csswring,
+    autoprefixer,
+    postcssPresetEnv({ stage: 0 })
+  ];
+
+  return gulp.src('./scss/style.scss')
     .pipe(sass())
-    .pipe(gulp.dest("css"))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dist'))
     .pipe(browserSync.stream());
 });
 
 // Watch Sass & Serve
-gulp.task("serve", ["sass"], function() {
+gulp.task("serve", ["styles"], function () {
   browserSync.init({
     server: "./"
   });
 
-  gulp.watch(["scss/*.scss"], ["sass"]);
+  gulp.watch(["scss/*.scss"], ["styles"]);
   gulp.watch("*.html").on("change", browserSync.reload);
   gulp.watch("js/*.js").on("change", browserSync.reload);
 });
